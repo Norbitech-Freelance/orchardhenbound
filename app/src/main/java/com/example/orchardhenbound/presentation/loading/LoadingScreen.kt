1639531d7bd86c9f85409933ffcbfe842e9d8df5
Artem Absaliamov <sxallordd@gmail.com>
2026-01-21 17:44:27 +0200

    pravka 15

--- app/src/main/java/com/example/orchardhenbound/presentation/loading/LoadingScreen.kt
+++ app/src/main/java/com/example/orchardhenbound/presentation/loading/LoadingScreen.kt
@@ -2,8 +2,10 @@ package com.example.orchardhenbound.presentation.loading
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableIntStateOf
@@ -11,6 +13,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -20,6 +23,7 @@ import kotlinx.coroutines.delay
 
 @Composable
 fun LoadingScreen(onFinished: () -> Unit) {
+
     val frames = listOf(
         R.drawable.loading_1,
         R.drawable.loading_2,
@@ -30,16 +34,28 @@ fun LoadingScreen(onFinished: () -> Unit) {
 
     LaunchedEffect(Unit) {
         val stepMs = 400L
-
         for (i in frames.indices) {
             frameIndex.intValue = i
             delay(stepMs)
         }
-
         onFinished()
     }
 
-    Box(modifier = Modifier.fillMaxSize()) {
+    // базовые размеры макета
+    val baseW = 412f
+    val baseH = 892f
+    val loadingW = 152f
+    val loadingH = 24f
+    val ratio = loadingW / loadingH
+
+    BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
+        val density = LocalDensity.current
+
+        val screenW = with(density) { maxWidth.toPx() }
+        val screenH = with(density) { maxHeight.toPx() }
+
+        fun x(v: Float) = (screenW * (v / baseW)).dp
+
         FullScreenBackground(
             backgroundRes = R.drawable.bg_loading,
             contentDescription = stringResource(R.string.cd_loading_background)
@@ -50,7 +66,8 @@ fun LoadingScreen(onFinished: () -> Unit) {
             contentDescription = stringResource(R.string.cd_loading_animation),
             modifier = Modifier
                 .align(Alignment.Center)
-                .size(width = 152.dp, height = 24.dp),
+                .width(x(loadingW))
+                .aspectRatio(ratio),
             contentScale = ContentScale.Fit
         )
     }
